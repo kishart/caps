9aaf450843577521346e75fab0073370b4496992
peach <108270895+kishart@users.noreply.github.com>
2024-01-26 08:52:41 +0800

    Update welcome.blade.php wla pa nahuman

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,3 +93,88 @@
     </div>
 </div>
 @endsection
+
+
+
+
+
+
+<!--
+tempo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+  <div class="hero">
+  <div class="form-box">
+  <div class="button-box">
+    <div id="btn"></div>
+    <button type="button" class="toggle-btn">Log In</button>
+    <button type="button" class="toggle-btn">Register</button>
+    </div>
+   
+    <form class="input-group">
+      <input type="text" class="input-field" placeholder="User Id" required>
+      <input type="text" class="input-field" placeholder="Enter Password" required>
+      <button type="submit" class="submit-btn">Log In</button>
+    </div>
+    
+    </div>
+</body>
+</html>
+
+*{
+  margin:0;
+  padding:0;
+}
+.hero{
+  height:100%;
+  width: 100%;
+  background-color:pink;
+  background-position:center;
+  background-size:cover;
+  position:absolute;
+}
+.form-box{
+   width:380px;
+  height:480px;
+  position:relative;
+  margin:6% auto;
+  background:#fff;
+  padding:5px;
+}
+.button-box{
+  width:220px;
+  margin:35px auto;
+  position:relative;
+  box-shadow:0 0 20px 9px #ff61241f;
+  border-radius:30px;
+}
+.toggle-btn{
+  padding:10px 30px;
+  cursor:pointer;
+  background:transparent;
+  border:0;
+  outline:none;
+  position:relative;
+}
+#btn{
+  top:0;
+  left:0;
+  position:absolute;
+  width:110px;
+  height:100%;
+  background: linear-gradient(to right, #ff105f, #ffad06);
+  border-radius: 30px;
+  transition: .5s;
+}
+
+link:https://www.youtube.com/watch?v=L5WWrGMsnpw
+9:22
+
+-->
