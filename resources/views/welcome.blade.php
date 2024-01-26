f0acbf423a40034776b4e713442330519270e9d1
kishart <amoralyse21@gmail.com>
2024-01-26 21:38:37 +0800

    wait sa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,3 +93,36 @@
     </div>
 </div>
 @endsection
+
+
+
+
+
+
+<div class="hero">
+    <div class="form-box">
+    <div class="button-box">
+      <div id="btn"></div>
+      <button type="button" class="toggle-btn" onclick="login()">Log In</button>
+      <button type="button" class="toggle-btn" onclick="register()">Register</button>
+      </div>
+     
+      <form id="login" class="input-group">
+        <input type="text" class="input-field" placeholder="User Id" required>
+        <input type="text" class="input-field" placeholder="Enter Password" required>
+   
+        <button type="submit" class="submit-btn">Log In</button>
+           </form>
+      
+       <form id="register" class="input-group">
+        <input type="text" class="input-field" placeholder="User Id" required>
+         <input type="text" class="input-field" placeholder="Email Id" required>
+        <input type="text" class="input-field" placeholder="Enter Password" required>
+         
+   
+        <button type="submit" class="submit-btn">Log In</button>
+           </form>
+      </div>
+      
+      </div>
+  
