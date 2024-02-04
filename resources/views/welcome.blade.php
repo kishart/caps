7c2bb542e34ff97490a9bf268f9dacc5bd63efa0
kishart <amoralyse21@gmail.com>
2024-02-04 09:13:08 +0800

    wa pa nagkadimao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,108 +3,154 @@
 <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
 
 <style>
+    .container {
+        display: flex;
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 20px;
+    }
 
-.container {
-            display: flex;
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 20px;
-        }
+    .column {
+        flex: 1;
+        padding: 20px;
+        margin: 10px;
+    }
 
-        .column {
-            flex: 1;
-            padding: 20px;
-            margin: 10px;
-        }
-        img{
-           width: 55%;
-           height: 75%;
-           margin-left: 12%;
-           margin-top: 20%;
-           border-radius: 15px; 
-        }
-        h1{
-            margin-left: 20%;
-            padding-top: 15%;
-            padding-bottom: 5%;
-            font-family: 'DM Serif Display', serif;
-        }
+    img {
+        width: 55%;
+        height: 60%;
+        margin-left: 12%;
+        margin-top: 20%;
+        border-radius: 15px;
+    }
 
-        #login{
-  left:50px;
-}
-#register {
-    left: 450px;
-    display: none;
-  }
-.form-box{
-   width:380px;
-  height:480px;
-  position:relative;
-  margin:6% auto;
-  background:#fff;
-  padding:5px;
-  overflow:hidden;
-}
-.button-box{
-  width:220px;
-  margin:35px auto;
-  position:relative;
-  box-shadow:0 0 20px 9px #ff61241f;
-  border-radius:30px;
-}
+    h1 {
+        margin-left: 20%;
+        padding-top: 15%;
+        padding-bottom: 5%;
+        font-family: 'DM Serif Display', serif;
+    }
 
 
-  .toggle-btn {
-    padding: 10px 30px;
-    cursor: pointer;
-    background: transparent;
-    border: 0;
-    outline: none;
-    position: relative;
-    transition: background 0.5s ease; /* Add transition effect */
-  }
-#btn{
-  top:0;
-  left:0;
-  position:absolute;
-  width:110px;
-  height:100%;
-  background: linear-gradient(to right, #ff105f, #ffad06);
-  border-radius: 30px;
-  transition: .5s;
-}
+    .form-box {
+        width: 380px;
+        height: 480px;
+        position: relative;
+        margin: 6% auto;
+        background: #fff;
+        padding: 5px;
+
+        overflow: hidden;
+    }
+
+    .button-box {
+        width: 220px;
+        margin: 35px auto;
+        position: relative;
+        box-shadow: 0 0 20px 9px #ff61241f;
+        border-radius: 30px;
+    }
+
+    .toggle-btn {
+        padding: 10px 30px;
+        cursor: pointer;
+        background: transparent;
+        border: 0;
+        outline: none;
+        position: relative;
+    }
+
+    #btn {
+        top: 0;
+        left: 0;
+        position: absolute;
+        width: 110px;
+        height: 100%;
+        background: linear-gradient(to right, #ff105f, #ffad06);
+        border-radius: 30px;
+        transition: .5s;
+    }
+
+    .input-group {
+        top: 10px;
+        position: absolute;
+        width: 280px;
+        transition: .5s;
+    }
+
+    .input-field {
+        width: 100%;
+        padding: 10px 0;
+        margin: 5px 0;
+        border-left: 0;
+        border-top: 0;
+        border-right: 0;
+        border-bottom: 1px solid #999;
+        outline: none;
+        background: transparent;
+    }
+
+    .submit-btn {
+        width: 85%;
+        padding: 10px 30px;
+        cursor: pointer;
+        display: block;
+        margin: auto;
+        background: linear-gradient(to right, #ff105f, #ffad06);
+        border: 0;
+        outline: none;
+        border-radius: 30px;
+    }
+
+    #login {
+        left: 50px;
+        padding-bottom: 30px;
+    }
+
+    #register {
+        left: 450px;
+        margin-bottom: 60px;
+    }
+
+    .forma {
+        display: flex;
+        height: 5vh;
+        margin-bottom: 40px;
+    }
 </style>
 
 
 @extends('layouts.welcome')
 @section('content')
-<div class="container">
-    <div class="column">
-        <img src="{{ asset('images/Logo.png') }}" alt="logo">
+    <div class="container">
+        <div class="column">
+            <img src="{{ asset('images/Logo.png') }}" alt="logo">
 
-    </div>
+        </div>
 
-                <div class="column">
-                    <form id="login" method="POST" action="{{ route('login') }}">
+        <div class="column">
+            <div class="form-box">
+                <div class="button-box">
+                    <div id="btn"></div>
+                    <button type="button" class="toggle-btn" onclick="login()">Log In</button>
+                    <button type="button" class="toggle-btn" onclick="register()">Register</button>
+                </div>
+                <div class="forma">
+
+                <form id="login" class="input-group" method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        
-                        <h1>Husnie Photography</h1>
-
-                        <div class="button-box">
-                            <div id="btn"></div>
-                            <button type="button" class="toggle-btn" onclick="login()">Log In</button>
-                            <button type="button" class="toggle-btn" onclick="register()">Register</button>
-                            </div>
-
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
+                            <label for="email"
+                                class="col-md-4 col-form-label text-md-end">{{ __('Email Address or Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" @error('username') is-invalid @enderror @error('email') is-invalid @enderror
-                                " name="username" value="{{old('username') }}" required autocomplete="username" autofocus>
-                                
+                                <input id="username" type="username"
+                                    class="form-control @error('username') is-invalid @enderror @error('email') is-invalid @enderror
+                            "
+                                    name="username" value="{{ old('username') }}" required autocomplete="username"
+                                    autofocus>
+
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -123,7 +169,9 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" @error('password') is-invalid @enderror" class="input-field" name="password" required autocomplete="current-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                                    autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -133,33 +181,44 @@
                             </div>
                         </div>
 
-                       
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-                                
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </form>
 
-
-                    <form id="register" method="POST" class="input-group" action="{{ route('register') }}">
+                    <form id="register" class="input-group" method="POST" action="{{ route('register') }}">
                         @csrf
-                        <h1>Husnie Photography</h1>
-
-                        <div class="button-box">
-                            <div id="btn"></div>
-                            <button type="button" class="toggle-btn" onclick="login()">Log In</button>
-                            <button type="button" class="toggle-btn" onclick="register()">Register</button>
-                            </div>
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text"  @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -170,10 +229,13 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email"
+                                class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" @error('email') is-invalid @enderror" class="input-field" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -187,7 +249,9 @@
                             <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username"  @error('username') is-invalid @enderror" class="input-field" name="username" value="{{ old('username') }}" required autocomplete="username">
+                                <input id="username" type="username"
+                                    class="form-control @error('username') is-invalid @enderror" name="username"
+                                    value="{{ old('username') }}" required autocomplete="username">
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -198,10 +262,13 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password"
+                                class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" @error('password') is-invalid @enderror" class="input-field" name="password" required autocomplete="new-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                                    autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -212,10 +279,12 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm"
+                                class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password"  name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
@@ -227,44 +296,27 @@
                             </div>
                         </div>
                     </form>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    </div>
 
-<script>
-  var x = document.getElementById("login");
-  var y = document.getElementById("register");
-  var loginBtn = document.getElementById("login-btn");
-  var registerBtn = document.getElementById("register-btn");
+    <script>
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
 
-  function register() {
-    x.style.left = "-400px";
-    y.style.left = "50px";
+        function register() {
+            x.style.left = "-250px";
+            y.style.left = "-130px";
+            z.style.left = "110px";
+        }
 
-    // Show the register form
-    y.style.display = "block";
-
-    // Hide the login form
-    x.style.display = "none";
-
-    // Update button styles
-    registerBtn.style.background = "linear-gradient(to right, #ff105f, #ffad06)";
-    loginBtn.style.background = "transparent";
-  }
-
-  function login() {
-    x.style.left = "50px";
-    y.style.left = "450px";
-
-    // Show the login form
-    x.style.display = "block";
-
-    // Hide the register form
-    y.style.display = "none";
-
-    // Update button styles
-    loginBtn.style.background = "linear-gradient(to right, #ff105f, #ffad06)";
-    registerBtn.style.background = "transparent";
-  }
-</script>
+        function login() {
+            x.style.left = "50px";
+            y.style.left = "550px";
+            z.style.left = "0";
+        }
+    </script>
 @endsection
-
