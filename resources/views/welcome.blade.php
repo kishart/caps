8c9efc6398b841b0f444b0083d14cf0f2d0c6b00
kishart <amoralyse21@gmail.com>
2024-02-04 09:17:24 +0800

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
 
                         <div class="row mb-3">
                             <label for="email"
-                                class="col-md-4 col-form-label text-md-end">{{ __('Email Address or Username') }}</label>
+                                class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username"
