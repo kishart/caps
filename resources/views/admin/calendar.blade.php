a1170141e4a95d309a195c915ddc3639ae3986fb
kishart <amoralyse21@gmail.com>
2024-06-03 20:50:05 +0800

    hinayhinay

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -14,7 +14,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
@@ -38,84 +38,69 @@
         </div>
     </div>
 
+    <form method="post" action="{{url('save-booking')}}">
+        @csrf
+        <div class="md-3">
+            <label class="form-label">Title:</label>
+            <input type="text" class="form-control" name="title" 
+                value="{{old('title')}}">
+            @error('title')
+            <div class="alert alert-danger" role="alert">
+                {{($message)}}
+            </div>
+            @enderror
+        </div>
 
+        <div class="md-3">
+            <label class="form-label">Start Date:</label>
+            <input type="date" class="form-control" name="start_date" 
+                value="{{old('start_date')}}">
+            @error('start_date')
+            <div class="alert alert-danger" role="alert">
+                {{($message)}}
+            </div>
+            @enderror
+        </div>
 
-
-      <form method="post" action="{{url('save-booking')}}">
-                    @csrf
-                    <div class="md-3">
-                        <label class="form-label">Title:</label>
-                        <input type="text" class="form-control" name="title" 
-                        value="{{old('title')}}">
-                        @error('title')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                        
-                    </div>
-
-                    <div class="md-3">
-                        <label class="form-label">Start Date:</label>
-                        <input type="date" class="form-control" name="start_date" 
-                        value="{{old('start_date')}}">
-                        @error('start_date')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                        
-                    </div>
-
-                    <div class="md-3">
-                        <label class="form-label">Start Date:</label>
-                        <input type="date" class="form-control" name="end_date" 
-                        value="{{old('end_date')}}">
-                        @error('end_date')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                        
-                    </div>
-                    <br>
-                    <button type="submit"class="button-28" role="button">Submit</button>
-                </form>
-
-
-
-
-
-
-
-
-
+        <div class="md-3">
+            <label class="form-label">Start Date:</label>
+            <input type="date" class="form-control" name="end_date" 
+                value="{{old('end_date')}}">
+            @error('end_date')
+            <div class="alert alert-danger" role="alert">
+                {{($message)}}
+            </div>
+            @enderror
+        </div>
+        <br>
+        <button type="submit"class="button-28" role="button">Submit</button>
+    </form>
 
     <script>
- $(document).ready(function() {
-    var booking = @json($events);
+        $(document).ready(function() {
+            var booking = @json($events);
 
-    // Convert event dates to include only the date portion
-    booking.forEach(function(event) {
-        event.start = event.start.substr(0, 10); // Assuming start date is in YYYY-MM-DD format
-        event.end = event.end.substr(0, 10); // Assuming end date is in YYYY-MM-DD format
-    });
+            // Convert event dates to include only the date portion
+            booking.forEach(function(event) {
+                event.start = event.start.substr(0, 10); // Assuming start date is in YYYY-MM-DD format
+                event.end = event.end.substr(0, 10); // Assuming end date is in YYYY-MM-DD format
+            });
 
-    $('#calendar').fullCalendar({
-        events: booking,
-        eventRender: function(event, element) {
-            element.css('background-color', 'red'); // Change the background color of the event to red
-            
-        }
-    });
-});
+            $('#calendar').fullCalendar({
+                events: booking,
+                viewRender: function(view, element) {
+                    // Get the header element
+                    var header = element.find('.fc-toolbar h2');
 
-
-
-
+                    // Replace the header text with weekdays
+                    header.text('Weekdays');
+                },
+                eventRender: function(event, element) {
+                    element.css('background-color', '#AC6F53'); // Change the background color of the event to red
+                }
+            });
+        });
     </script>
-
-    
 </body>
 
 </html>
