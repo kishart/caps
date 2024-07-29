4adfc1b8ddafb34b28e3e77d3980a6912cc75fac
kishart <amoralyse21@gmail.com>
2024-07-29 16:57:25 +0800

    gamay pa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0" />
@@ -36,11 +34,11 @@
 }
 .material-symbols-outlined{
     font-size:40px;
-}
-
-.dropdown {
+}.dropdown {
       position: relative;
       display: inline-block;
+
+      
     }
 
     .dropdown-content {
@@ -50,6 +48,7 @@
       min-width: 160px;
       box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
       z-index: 1;
+      margin-left: -70px; /* Adjust as needed */
     }
 
     .dropdown-content a {
@@ -79,24 +78,19 @@
             <li><a href="{{ asset('ucalen') }}">Calendar</a></li>
             <li><a href="#">Photos</a></li>
             <li><a href="#">Contact</a></li>
-            <li>
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-            </li>
+            
 
             <div class="dropdown">
                 <span class="material-symbols-outlined">account_circle</span>
                 <div class="dropdown-content">
                   <a href="#">Profile</a>
                   <a href="#">My Appointments</a>
-                  <a href="#">Logout</a>
+                  <a href="#" onclick="logout()">Logout</a>
                 </div>
               </div>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
-            </form>
+              </form>
             
         </ul>
     </nav>
@@ -105,6 +99,12 @@
     @yield('content')
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script>
+        function logout() {
+          document.getElementById('logout-form').submit();
+        }
+      </script>
 </body>
 
 </html>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>im home pages</h1>
+<h1>im home page</h1>
 @endsection
