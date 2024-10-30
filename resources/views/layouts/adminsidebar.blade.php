3dbf6aadafa91428c8d562040d44ed9a771aa848
kishart <amoralyse21@gmail.com>
2024-10-31 00:35:49 +0800

    d machange ang bg sa nav

--- resources/views/layouts/adminsidebar.blade.php
+++ resources/views/layouts/adminsidebar.blade.php
@@ -47,7 +47,7 @@
     </button>
 
     <!-- Sidebar -->
-    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-58 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-70 h-screen transition-transform -translate-x-full sm:translate-x-0 custom-bg-brown"  aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-primary flex flex-col">
             <ul class="space-y-2 font-medium flex-grow">
                 <div class="flex justify-center">
